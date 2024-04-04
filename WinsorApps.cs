6c3e183f677328e7cf30f04e65c76045971509ac
sophiejkim <158072125+sophiejkim@users.noreply.github.com>
2024-04-04 11:31:40 -0400

    Create WinsorApps.cs

---
+++ WinsorApps.cs
@@ -0,0 +1,9 @@
+namespace CS_First_HTTP_Client;
+
+public readonly record struct Login(string email, string password);
+
+public readonly record struct ErrorResponse(string email, string password);
+
+public readonly record struct AuthResponse(string UserId, string jwt, DateTime expires, string refreshToken);
+
+
